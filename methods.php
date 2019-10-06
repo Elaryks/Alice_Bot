0c481b79fac6b7dda6efba15ed2b30ad89a3d5a9
YSitnickov <jaroslav.sitnickov@yandex.ru>
2019-10-06 16:43:02 +0300

    getting the name

--- methods.php
+++ methods.php
@@ -16,7 +16,8 @@ function SendMessage($from_id, $message)
 {
     global $botToken;
     $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$from_id}&{$botToken}&v=5.101"));
-    $user_name = $user_info->response[0]->first_name;
+    //$user_name = $user_info->response[0]->first_name;
+    $user_name = $user_info['response'][0]['first_name'];
     wh_log($user_name);
     $msg = $user_name . ", " . $message;
     $request_params = array(
