4bafc9b9712423d0286bec1b70132ef7819d8e14
YSitnickov <jaroslav.sitnickov@yandex.ru>
2019-10-06 16:15:01 +0300

    variable error fix

--- get.php
+++ get.php
@@ -9,7 +9,7 @@ if (!isset($_REQUEST)) {
 
 $userdata = json_decode(file_get_contents('php://input'));
 
-if (strcmp($data->secret, $secretKey) !== 0 && strcmp($data->type, 'confirmation') !== 0) {
+if (strcmp($userdata->secret, $secretKey) !== 0 && strcmp($userdata->type, 'confirmation') !== 0) {
     die(json_encode(array("response" => 0, "error" => array("error_id" => 1, "error_message" => "Bad secret key"))));
 }
 
