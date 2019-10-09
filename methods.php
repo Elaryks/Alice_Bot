64b5723e6cfca4fed3ca707c03450b1c675fc677
YSitnickov <jaroslav.sitnickov@yandex.ru>
2019-10-09 20:36:10 +0300

    fix

--- methods.php
+++ methods.php
@@ -29,10 +29,12 @@ function GetUserInfo() // $info
 {
     global $user_id, $botToken;
     $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&fields=city,country&access_token={$botToken}&lang=en&v=5.101"), true);
-    $user_city = $user_info['response'][0]['city']['title'];
+    $user_city[0] = $user_info['response'][0]['city']['title'];
     if (strbool(empty($user_city)) === "true") {
         return "Кажется, у Вас не указан город в настройках &#128532; К сожалению, пока что я не могу сообщить Вам погоду.";
     }
+    $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&fields=city,country&access_token={$botToken}&lang=ru&v=5.101"), true);
+    $user_city[1] = $user_info['response'][0]['city']['title'];
     // $user_country = $user_info['response'][0]['country']['title'];
     return $user_city;
 }
@@ -40,17 +42,22 @@ function GetUserInfo() // $info
 function GetWeather()
 {
     $user_city = GetUserInfo();
-    $weather_info = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q={$user_city}&units=metric&APPID=3d1a65cde09ddedde287f0a4a4fa39e7"), true);
+    $weather_info = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q={$user_city[0]}&units=metric&APPID=3d1a65cde09ddedde287f0a4a4fa39e7"), true);
     if ($weather_info['cod'] == '404') {
         return ("Извини, я почему-то не смог определить температуру в твоём городе, но мой разработчик уже работает над этой проблемой &#128521;");
     }
-    $temperature = round(floatval($weather_info['main']['temp']));
-    $pressure = $weather_info['main']['pressure'];
-    $humidity = $weather_info['main']['humidity'];
-    return "По данным OpenWeatherMap в твоём городе ({$user_city}, ю ноу) вот такая погода:
+    $temperature = round(floatval($weather_info['main']['temp'])); // Температура
+    $pressure = $weather_info['main']['pressure']; // Давление
+    $humidity = $weather_info['main']['humidity']; // Влажность воздуха
+    $windspeed = $weather_info['wind']['speed']; // Скорость ветра
+    $winddirection = $weather_info['wind']['deg']; // Направление ветра
+    $wd = array('северный', 'северо-восточный', 'восточный', 'юго-восточный', 'южный', 'юго-западный', 'западный', 'северо-западный');
+    $winddirection = $wd[(floor(($winddirection / 45) + 1) % 8)] . " ({$winddirection}°)";
+    return "По данным OpenWeatherMap в твоём городе ({$user_city[1]}, если я не ошибаюсь) вот такая погода:
             Температура: {$temperature}°C
             Давление: {$pressure} мм рт. ст.
-            Влажность: {$humidity}%";
+            Влажность: {$humidity}%
+            Ветер {$winddirection}, $windspeed м/с";
 }
 
 function UploadPhoto()
