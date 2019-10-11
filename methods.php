04a953d8af91d976d511feb2b2680ef8309a472b
YSitnickov <jaroslav.sitnickov@yandex.ru>
2019-10-11 16:25:40 +0300

    fix

--- methods.php
+++ methods.php
@@ -71,6 +71,14 @@ function GetWeather()
 
 function UploadPhoto()
 {
+    $host = 'localhost'; // имя хоста (уточняется у провайдера)
+    $database = 'TheAliceBot'; // имя базы данных, которую вы должны создать
+    $user = 'jaroks'; // заданное вами имя пользователя, либо определенное провайдером
+    $pswd = '200926200926jarok%'; // заданный вами пароль
+
+    $dbh = mysql_connect($host, $user, $pswd) or die("Не могу соединиться с MySQL.");
+    logging('dbh: ' . $dbh);
+    mysql_select_db($database) or die("Не могу подключиться к базе.");
     return;
 }
 
